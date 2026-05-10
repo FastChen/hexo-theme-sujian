631e22046fe4444397c857e13966422b713802a7
FastChen <fastchen@nullcraft.org>
2026-05-10 23:44:36 +0800

    v1.2.0 (2026-05-10)
    [更新]
    - 文章内右下角悬浮目录(toc)功能
    - 文章标题下方增加显示标签功能
      - 可通过 `_config.yml` -> `post.show_tags` 控制显示
    
    [优化]
    - `languages` 的语言文件结构

--- source/js/utils/back-to-top.js
+++ source/js/utils/back-to-top.js
@@ -1,15 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var backToTop = document.getElementById('back-to-top');
+// document.addEventListener('DOMContentLoaded', function() {
+//   var backToTop = document.getElementById('back-to-top');
 
-  window.addEventListener('scroll', function() {
+//   window.addEventListener('scroll', function() {
+//     if (window.pageYOffset > 300) {
+//       backToTop.classList.add('show');
+//     } else {
+//       backToTop.classList.remove('show');
+//     }
+//   });
+
+//   backToTop.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth'
+//     });
+//   });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  var backToTop = document.getElementById('back-to-top');
+  if (!backToTop) return;
+
+  window.addEventListener('scroll', function () {
     if (window.pageYOffset > 300) {
-      backToTop.classList.add('show');
+      backToTop.classList.remove('d-none');
     } else {
-      backToTop.classList.remove('show');
+      backToTop.classList.add('d-none');
     }
   });
 
-  backToTop.addEventListener('click', function(e) {
+  backToTop.addEventListener('click', function (e) {
     e.preventDefault();
     window.scrollTo({
       top: 0,
