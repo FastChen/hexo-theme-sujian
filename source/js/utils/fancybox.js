06dff20ff0fd21d62dd13a2d48fe7cacdfd051de
FastChen <fastchen@nullcraft.org>
2026-05-19 01:38:37 +0800

    v1.2.2 (2026-05-19)
    [更新]
    - 使用 Hexo 多语言功能实现自定义 Fancybox 文本

--- source/js/utils/fancybox.js
+++ source/js/utils/fancybox.js
@@ -21,8 +21,23 @@ document.addEventListener('DOMContentLoaded', function () {
     Hash: false,
     Thumbs: false,
     dragToClose: true,
-    Image: { zoom: true }
+    Image: { zoom: true },
+    l10n: {
+      NEXT: window.FANCYBOX_I18N.NEXT,
+      PREV: window.FANCYBOX_I18N.PREV,
+      ZOOM_IN: window.FANCYBOX_I18N.ZOOM_IN,
+      ZOOM_OUT: window.FANCYBOX_I18N.ZOOM_OUT,
+      TOGGLE_FULL: window.FANCYBOX_I18N.TOGGLE_FULL,
+      ROTATE_CCW: window.FANCYBOX_I18N.ROTATE_CCW,
+      ROTATE_CW: window.FANCYBOX_I18N.ROTATE_CW,
+      FLIP_X: window.FANCYBOX_I18N.FLIP_X,
+      FLIP_Y: window.FANCYBOX_I18N.FLIP_Y,
+      RESET: window.FANCYBOX_I18N.RESET,
+      TOGGLE_AUTOPLAY: window.FANCYBOX_I18N.TOGGLE_AUTOPLAY,
+      TOGGLE_FULLSCREEN: window.FANCYBOX_I18N.TOGGLE_FULLSCREEN,
+      TOGGLE_THUMBS: window.FANCYBOX_I18N.TOGGLE_THUMBS,
+      CLOSE: window.FANCYBOX_I18N.CLOSE,
+      IMAGE_ERROR: window.FANCYBOX_I18N.IMAGE_ERROR,
+    },
   });
-
-  console.log('Fancybox v6 已初始化');
 });
